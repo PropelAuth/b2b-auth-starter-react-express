73e06b294a10177b39fd2b29897ef6d5e47c0f1a
Andrew <andrew@propelauth.com>
2021-10-10 17:21:57 -0700

    Add authenticated HTTP request to /api/whoami

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import AuthenticationButtons from "./AuthenticationButtons";
+import ComponentThatCallsAnApi from "./ComponentThatCallsAnApi";
 import OrganizationView from "./OrganizationView";
 
 export default function App() {
-    return <div>
-        <AuthenticationButtons/>
-        <OrganizationView/>
-    </div>
+  return <div>
+    <AuthenticationButtons/><hr/>
+    <ComponentThatCallsAnApi/><hr/>
+    <OrganizationView/>
+  </div>
 }
 
